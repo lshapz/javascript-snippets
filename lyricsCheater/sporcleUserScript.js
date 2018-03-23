2f1c7bc1022aac0fae4ddb78a7ecd0d68ad4d4b2
Laura Shapiro <laurashapiro01@gmail.com>
2018-03-23 01:27:35 -0400

    updated script to 0.2 and it works again

--- lyricsCheater/sporcleUserScript.js
+++ lyricsCheater/sporcleUserScript.js
@@ -1,41 +1,33 @@
 // ==UserScript==
 // @name         Sporcle Lyrics Quiz Semi-Cheater
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    http://github.com/lshapz
+// @version      0.2
 // @description  automatically populates a lyrics quiz on sporcle with 87 common words
 // @author       lshapz
-// @match        http://*/*
+// @match        http://*.sporcle.com/*
+// @match        https://*.sporcle.com/*
 // @grant        none
 // ==/UserScript==
+
 if (document.querySelectorAll("a[href='/games/subcategory/lyrics']") && document.getElementById('gameinput')){
-
-
     window.addEventListener("onload",load);
     window.addEventListener("load",load,true);
-
  }
 
  function load(){
      sporcle();
  }
 
-
 function sporcle(){
-const words = ["the", "a", "and", "I", "is", "was", "am", "were", "are", "arent", "werent", "love", "Im", "Id", "Ill", "Ive", "You", "Your", "Youre", "Youve", "Youd", "He", "Her", "She", "Him", "They", "Theyd", "Theyll", "Theyve", "Their", "We", "Us", "Our", "Me", "My", "Mine", "This", "That", "Here", "There", "Who", "What", "Where", "When", "Why", "How", "in", "on", "if", "of", "off", "or", "for", "from", "to", "go", "get", "got", "with", "out", "without", "over", "under", "up", "down", "like", "as", "at", "an", "know", "see", "seen", "been", "do", "dont", "can", "cant", "will", "wont", "other", "not", "well", "just", "myself", "all", "it", "its"];
+  const words = ["the", "a", "and", "I", "is", "be", "was", "am", "were", "are", "arent", "werent", "love", "Im", "Id", "Ill", "Ive", "You", "Your", "Youre", "Youve", "Youd", "He", "Her", "She", "Him", "They", "Theyd", "Theyll", "Theyve", "Their", "We", "Us", "Our", "Me", "My", "Mine", "This", "That", "Here", "There", "Who", "What", "Where", "When", "Why", "How", "in", "on", "if", "of", "off", "or", "for", "from", "to", "go", "get", "got", "with", "out", "without", "over", "under", "up", "down", "like", "as", "at", "an", "know", "see", "seen", "been", "do", "dont", "can", "cant", "will", "wont", "other", "not", "well", "just", "myself", "all", "it", "its"];
+  var input = document.getElementById('gameinput');
+  var event = new Event('input');
 
-
-words.forEach(word=>{
-  setTimeout(3000);
-  var input =  document.getElementById('gameinput');
-  input.value = word;
-  var evt = document.createEvent("KeyboardEvent");
-  evt.initKeyboardEvent ("keyup", true, true, window,
-                false, false, false, false,
-                32, 32); 
-
-  input.dispatchEvent(evt);
-
-});}
+  words.forEach(word=>{
+     input.value = word;
+     input.dispatchEvent(event);
+  });
+}
 
 
 //https://greasyfork.org/en/scripts/24883-sporcle-lyrics-quiz-semi-cheater
